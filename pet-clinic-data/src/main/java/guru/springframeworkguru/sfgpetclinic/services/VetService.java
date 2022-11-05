b56003dd1ea3af42b2c537ca2c983a9cf12bd76c
Magdalena <mkceva@gmail.com>
2022-11-05 23:38:24 +0100

    imlement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframeworkguru/sfgpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package guru.springframeworkguru.sfgpetclinic.services;
+
+import guru.springframeworkguru.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
