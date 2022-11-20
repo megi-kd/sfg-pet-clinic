a764f1a6b65970d34adaff29c1798b7526eefd04
Magdalena <mkceva@gmail.com>
2022-11-21 00:01:16 +0100

    adding list all vets on Vet index page closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframeworkguru.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets"})
-    public String list(){
+    public String list(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/list";
     }
 }
